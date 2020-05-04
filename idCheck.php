edd1723455293ed6a5aaceb56943e57b6ffb0a24
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2020-05-05 03:31:00 +0900

    아이디 중복 확인

--- idCheck.php
+++ idCheck.php
@@ -1,3 +1,15 @@
 <?php
-    
+    $id = $_GET['id'];
+
+    $conn = mysqli_connect('127.0.0.1', 'root', 'mirim2', 'toww');
+
+    /* 아이디 중복 확인 */
+    $sql = 'select * from members where id="'.$id.'"';
+    $result = mysqli_query($conn, $sql);
+
+    if(mysqli_num_rows($result) > 0) {
+        echo '<script>alert("중복된 아이디가 있습니다. 다른 아이디를 입력해주세요."); window.close();</script>';
+    } else {
+        echo '<script>alert("사용 가능한 아이디입니다!"); window.close();</script>';
+    }
 ?>
