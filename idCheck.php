3644c0f0d969339109fe75af27a30578a48cfeea
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2020-05-23 23:04:46 +0900

    utf-8 형식으로 변환

--- idCheck.php
+++ idCheck.php
@@ -1,15 +1,23 @@
-<?php
-    $id = $_GET['id'];
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    </head>
 
-    $conn = mysqli_connect('localhost', 'toww', 'mirimww1!', 'toww');
+    <body>
+    <?php
+        $id = $_GET['id'];
 
-    /* 아이디 중복 확인 */
-    $sql = 'select * from members where id="'.$id.'"';
-    $result = mysqli_query($conn, $sql);
+        $conn = mysqli_connect('localhost', 'toww', 'mirimww1!', 'toww');
 
-    if(mysqli_num_rows($result) > 0) {
-        echo '<script>alert("중복된 아이디가 있습니다. 다른 아이디를 입력해주세요."); window.close();</script>';
-    } else {
-        echo '<script>alert("사용 가능한 아이디입니다!"); window.close();</script>';
-    }
-?>
+        /* 아이디 중복 확인 */
+        $sql = 'select * from members where id="'.$id.'"';
+        $result = mysqli_query($conn, $sql);
+
+        if(mysqli_num_rows($result) > 0) {
+            echo '<script>alert("중복된 아이디가 있습니다. 다른 아이디를 입력해주세요."); window.close();</script>';
+        } else {
+            echo '<script>alert("사용 가능한 아이디입니다!"); window.close();</script>';
+        }
+    ?>
+    </body>
+</html>
